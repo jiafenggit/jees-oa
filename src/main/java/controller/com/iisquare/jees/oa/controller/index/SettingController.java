bc35859fc589148d51964d1693c2d89f09060459
iisquare <iisquare@163.com>
2014-09-10 16:28:43 +0800

    添加相关控制器

---
+++ src/main/java/controller/com/iisquare/jees/oa/controller/index/SettingController.java
@@ -0,0 +1,17 @@
+package com.iisquare.jees.oa.controller.index;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import com.iisquare.jees.core.component.CPermitController;
+
+/**
+ * 配置管理
+ * @author Ouyang <iisquare@163.com>
+ *
+ */
+@Controller
+@Scope("prototype")
+public class SettingController extends CPermitController {
+
+}
