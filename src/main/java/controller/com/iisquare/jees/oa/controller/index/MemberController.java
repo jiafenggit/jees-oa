767326e5f569456b392b3131f69989e980113b2e
iisquare <iisquare@163.com>
2014-10-16 16:44:32 +0800

    用户管理部分功能

--- src/main/java/controller/com/iisquare/jees/oa/controller/index/MemberController.java
+++ src/main/java/controller/com/iisquare/jees/oa/controller/index/MemberController.java
@@ -1,11 +1,15 @@
 package com.iisquare.jees.oa.controller.index;
 
+import java.util.Collection;
+import java.util.Map;
+
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.iisquare.jees.core.component.PermitController;
 import com.iisquare.jees.framework.util.DPUtil;
+import com.iisquare.jees.framework.util.ServletUtil;
 import com.iisquare.jees.framework.util.ValidateUtil;
 import com.iisquare.jees.oa.domain.Member;
 
@@ -17,6 +21,70 @@ import com.iisquare.jees.oa.domain.Member;
 @Controller
 @Scope("prototype")
 public class MemberController extends PermitController {
+	
+	public String layoutAction() throws Exception {
+		return displayTemplate();
+	}
+	
+	public String listAction () throws Exception {
+		int page = ValidateUtil.filterInteger(get("page"), true, 0, null, null);
+		int pageSize = ValidateUtil.filterInteger(get("rows"), true, 0, 500, null);
+		Map<Object, Object> map = memberService.search(ServletUtil.singleParameterMap(request), "sort desc", page, pageSize);
+		assign("total", map.get("total"));
+		assign("rows", DPUtil.collectionToArray((Collection<?>) map.get("rows")));
+		return displayJSON();
+	}
+	
+	public String editAction() throws Exception {
+		Integer id = ValidateUtil.filterInteger(get("id"), true, 0, null, null);
+		Member info;
+		if(DPUtil.empty(id)) {
+			info = new Member();
+		} else {
+			info = memberService.getById(id);
+			if(DPUtil.empty(info)) return displayInfo("信息不存在，请刷新后再试", null);
+		}
+		assign("info", info);
+		assign("statusMap", memberService.getStatusMap(false));
+		return displayTemplate();
+	}
+	
+	public String saveAction() throws Exception {
+		Integer id = ValidateUtil.filterInteger(get("id"), true, 0, null, null);
+		Member persist;
+		if(DPUtil.empty(id)) {
+			persist = new Member();
+		} else {
+			persist = memberService.getById(id);
+			if(DPUtil.empty(persist)) return displayMessage(3001, "信息不存在，请刷新后再试");
+		}
+		long time = System.currentTimeMillis();
+		persist.setUpdateId(currentMember.getId());
+		persist.setUpdateTime(time);
+		int result;
+		if(DPUtil.empty(persist.getId())) {
+			persist.setCreateId(currentMember.getId());
+			persist.setCreateTime(time);
+			result = memberService.insert(persist);
+		} else {
+			result = memberService.update(persist);
+		}
+		if(result > 0) {
+			return displayMessage(0, url("layout"));
+		} else {
+			return displayMessage(500, "操作失败");
+		}
+	}
+	
+	public String deleteAction() throws Exception {
+		Object[] idArray = DPUtil.explode(get("ids"), ",", " ", true);
+		int result = memberService.delete(idArray);
+		if(result > 0) {
+			return displayInfo("操作成功", url("layout"));
+		} else {
+			return displayInfo("操作失败，请刷新后再试", null);
+		}
+	}
 	
 	@RequestMapping(value="/login")
 	public String loginAction() throws Exception {
--- src/main/java/model/com/iisquare/jees/oa/service/MemberService.java
+++ src/main/java/model/com/iisquare/jees/oa/service/MemberService.java
@@ -1,6 +1,8 @@
 package com.iisquare.jees.oa.service;
 
+import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,9 @@ import com.iisquare.jees.framework.controller.ControllerBase;
 import com.iisquare.jees.framework.model.ServiceBase;
 import com.iisquare.jees.framework.util.CodeUtil;
 import com.iisquare.jees.framework.util.DPUtil;
+import com.iisquare.jees.framework.util.ServiceUtil;
+import com.iisquare.jees.framework.util.SqlUtil;
+import com.iisquare.jees.framework.util.ValidateUtil;
 import com.iisquare.jees.oa.dao.MemberDao;
 import com.iisquare.jees.oa.domain.Member;
 
@@ -19,15 +24,74 @@ public class MemberService extends ServiceBase {
 	@Autowired
 	public MemberDao memberDao;
 	
-	public Map<String, String> getStatusMap() {
+	public Map<String, String> getStatusMap(boolean bAll) {
 		Map<String, String> map = new LinkedHashMap<String, String>();
-		map.put("-1", "已删除");
+		if(bAll) {
+			map.put("-1", "已删除");
+		}
 		map.put("0", "禁用");
 		map.put("1", "正常");
 		return map;
 	}
 	
 	public MemberService() {}
+	
+	public Map<Object, Object> search(Map<String, String> map, String orderBy, int page, int pageSize) {
+		StringBuilder sb = new StringBuilder("select * from ")
+			.append(memberDao.tableName()).append(" where 1 = 1");
+		Map<String, Object> paramMap = new HashMap<String, Object>();
+		Object title = map.get("title");
+		if(!DPUtil.empty(title)) {
+			sb.append(" and title like :title");
+			paramMap.put("title", DPUtil.stringConcat("%", title, "%"));
+		}
+		int typeId = ValidateUtil.filterInteger(map.get("typeId"), true, 0, null, 0);
+		if(!DPUtil.empty(typeId)) {
+			sb.append(" and type_id = :typeId");
+			paramMap.put("typeId", typeId);
+		}
+		String serial = map.get("serial");
+		if(!DPUtil.empty(serial)) {
+			sb.append(" and operate_id in (select ").append(memberDao.getPrimaryKey())
+				.append(" from ").append(memberDao.tableName()).append(" where serial = :serial)");
+			paramMap.put("serial", serial);
+		}
+		if(!DPUtil.empty(orderBy)) sb.append(" order by ").append(orderBy);
+		String sql = sb.toString();
+		int total = memberDao.getCount(sql, paramMap, true);
+		sql = DPUtil.stringConcat(sql, SqlUtil.buildLimit(page, pageSize));
+		List<Map<String, Object>> rows = memberDao.npJdbcTemplate().queryForList(sql, paramMap);
+		rows = ServiceUtil.fillFields(rows, new String[]{"status"}, new Map<?, ?>[]{getStatusMap(true)}, null);
+		rows = ServiceUtil.fillRelations(rows, memberDao, new String[]{"create_id", "update_id"}, new String[]{"serial", "name"}, null);
+		return DPUtil.buildMap(new String[]{"total", "rows"}, new Object[]{total, rows});
+	}
+	
+	public List<Member> getList(Map<String, Object> where, Map<String, String> operators, String orderBy, int page, int pageSize) {
+		String append = null;
+		if(!DPUtil.empty(orderBy)) append = DPUtil.stringConcat(" order by ", orderBy);
+		List<Member> list = memberDao.getList(where, operators, append, page, pageSize);
+		return list;
+	}
+	
+	public Map<String, Object> getById(Object id, boolean bFill) {
+		Map<String, Object> map = memberDao.getById("*", id);
+		if(null != map && bFill) {
+			map = ServiceUtil.fillRelations(map, memberDao, new String[]{"create_id", "update_id"}, new String[]{"serial", "name"}, null);
+		}
+		return map;
+	}
+	
+	public int insert(Member persist) {
+		return memberDao.insert(persist);
+	}
+	
+	public int update(Member persist) {
+		return memberDao.update(persist);
+	}
+	
+	public int delete(Object... ids) {
+		return memberDao.deleteByIds(ids);
+	}
 	
 	public Member getBySerial(String serial) {
 		return memberDao.getByField("serial", serial, null, null);
