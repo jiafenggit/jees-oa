0c81f275388bdcff55c4dd0c1816a35ce58496ab
iisquare <iisquare@163.com>
2014-10-20 18:15:25 +0800

    服务器信息管理

---
+++ src/main/java/controller/com/iisquare/jees/oa/controller/index/ServerController.java
@@ -0,0 +1,21 @@
+package com.iisquare.jees.oa.controller.index;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import com.iisquare.jees.core.component.PermitController;
+
+/**
+ * 服务器信息管理
+ * @author Ouyang <iisquare@163.com>
+ *
+ */
+@Controller
+@Scope("prototype")
+public class ServerController extends PermitController {
+	
+	public String infoAction() throws Exception {
+		return displayTemplate();
+	}
+	
+}
